72ecf844c4b94913f1fa3fa6f9744793d9baf579
SAIPHER ZURETI <saipherzureti@gmail.com>
2019-07-12 12:10:51 -0500

    Finished creating Cards

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,46 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+//CONTAINER
+const cards = document.querySelector('.cards-container');
+
+//AXIOS
+const promise2 = axios.get('https://lambda-times-backend.herokuapp.com/articles');
+
+//PROMISE
+promise2 
+    .then(info => {
+        console.log('Cards Promise Working Fine', info.data);
+    })
+//CATCH
+    .catch(error => {
+        console.log('ERROR: Cards Promise Not Working', error);
+    })
+
+//FUNCTION FO CREATE NEW ELEMENTS
+function createCards(){
+    const card = document.createElement('div')
+    const headline = document.createElement('div')
+    const author = document.createElement('div')
+    const imgContainer = document.createElement('div')
+    const imgs = document.createElement('img')
+    const span =  document.createElement('span')
+
+//CREATE CONTENT
+card.classList.add('card');
+headline.classList.add('headline');
+author.classList.add('author');
+imgContainer.classList.add('img-container');
+span.classList.add('span');
+
+//IMAGE
+imgs.src = imgUrl;
+
+//CHANGE DISPLAY NAME FOR SPAN TO BY
+span.textContent = "By";
+
+//APPEND
+card.append(headline, author, imgContainer, span);
+imgContainer.appendChild(imgs);
+}
